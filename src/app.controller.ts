c56ec74e49f8e71deba1b77178a86b535090c4be
rkisztian <radeczki.krisztian@gmail.com>
2023-01-24 11:31:07 +0100

    Kész vannak a végpontok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,43 @@
 import {
+  Body,
   Controller,
   Get,
   Render,
+  Post,
+  Delete,
+  Param,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+
+import TarhelyDataDto from './tarhelydata.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Get('/api/tarhely')
+  async allTarhely() {
+    const[tarhely] = await db.execute(
+      'SELECT id, nev, meret, ar FROM tarhelycsomagok'
+    );
+    return { tarhely: tarhely};
   }
+
+  @Post('/api/tarhely')
+  async newTarhelyApi(@Body() tarhelydata : TarhelyDataDto ) {
+    await db.execute('INSERT INTO tarhelycsomagok (nev, meret, ar) VALUES (?, ?, ?)', [
+      tarhelydata.nev,
+      tarhelydata.meret,
+      tarhelydata.ar,
+    ]);
+  }
+
+  @Delete('/api/tarhely/:id')
+  async deletTarhelyApi(@Param('id') id: number) {
+    await db.execute('DELETE FROM tarhelycsomagok WHERE id = ?', 
+    [id]
+    );
+  }
+
 }
---
+++ tarhely.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tarhelycsomagok (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nev VARCHAR(1000) NOT NULL,
+    meret INT NOT NULL,
+    ar INT NOT NULL
+)
+
